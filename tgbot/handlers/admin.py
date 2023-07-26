2dc4ad33b0b620df0a0f9229a88eb77652f441dd
edoxa1 <vip.aidyn1@gmail.com>
2023-07-26 23:19:16 +0600

    fixed parser bugs

--- tgbot/handlers/admin.py
+++ tgbot/handlers/admin.py
@@ -1,10 +1,24 @@
 from aiogram import Dispatcher
 from aiogram.types import Message
 
+from tgbot.services.parser import PdfParser
+
 
 async def admin_start(message: Message):
     await message.reply("Hello, admin!")
 
 
+async def update_schedule(message: Message):
+    await message.reply("Updating..")
+    if not message.get_args():
+        return
+    
+    abbr = message.get_args()
+    res = message.bot['config'].table.search_by_abbr(abbr)
+    text = '-----\n'.join(course.get_info() for course in res)
+    await message.answer(text)
+    
+
 def register_admin(dp: Dispatcher):
     dp.register_message_handler(admin_start, commands=["start"], state="*", is_admin=True)
+    dp.register_message_handler(update_schedule, commands=["update"], is_admin=True)
--- tgbot/services/parser.py
+++ tgbot/services/parser.py
@@ -2,7 +2,7 @@ from typing import List, Dict
 from tabula import read_pdf
 
 from tgbot.models.Course import Course
-from tgbot.models.Enums import Columns
+from tgbot.models.Enums import Column
 
 coursed_options = {'header': None,
                    'names': ['Course Abbr', 'S/T', 'Course Title',
@@ -19,60 +19,86 @@ class PdfParser:
     def load_schedule(self) -> List[Dict[str, any]]:
         # noinspection PyTypeChecker
         courses_json: List[Dict[str, any]] = read_pdf(self.path, pandas_options=coursed_options, pages='all',
-                                                      output_format='json')
+                                                      output_format='json', lattice=True, stream=False)
+        print(courses_json)
 
         return courses_json
 
-    def normalise_pdf_file(self) -> List[Course]:
+    def get_courses_list(self) -> List[Course]:
         courses: List[Course] = []
         for page in self.pdf_json:
             for row in page['data']:
-                temp = Course(self.get_abbr(row), self.get_ctype(row), self.get_title(row))
-                courses.append()
-
+                vars = (self._get_abbr(row), self._get_ctype(row), self._get_title(row),
+                self._get_credits_us(row), self._get_credits_eu(row), self._get_start_date(row),
+                self._get_end_date(row), self._get_weekdays(row), self._get_time(row),
+                self._get_enrolled(row), self._get_course_capacity(row),
+                self._get_faculty(row), self._get_room(row))  # WHAT THE FUCK
+                
+                (abbr, ctype, title, cus, ceu, start_date, end_date, days, times, enr, cap, faculty, room) = vars
+                
+                if not abbr:
+                    last_course = courses.pop()
+                    last_course.weekdays = last_course.weekdays.join(days)
+                    last_course.time = last_course.time.join(times)
+                    last_course.faculty = last_course.faculty.join(faculty)
+                    last_course.room = last_course.room.join(room)
+                    courses.append(last_course)
+                    continue
+                
+                temp = Course(abbr, ctype, title, cus, ceu, start_date, end_date, days, times, enr, cap, faculty, room)
+                
+                courses.append(temp)
+        
         return courses
 
-    def get_abbr(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.ABBR)
+    def _get_abbr(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.ABBR)
 
-    def get_ctype(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.CTYPE)
+    def _get_ctype(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.CTYPE)
 
-    def get_title(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.TITLE)
+    def _get_title(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.TITLE)
 
-    def get_credits_us(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.CRED_US)
+    def _get_credits_us(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.CRED_US)
 
-    def get_credits_eu(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.CRED_EU)
+    def _get_credits_eu(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.CRED_EU)
 
-    def get_start_date(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.START)
+    def _get_start_date(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.START)
 
-    def get_end_date(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.END)
+    def _get_end_date(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.END)
 
-    def get_weekdays(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.DAYS)
+    def _get_weekdays(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.DAYS)
 
-    def get_time(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.TIME)
+    def _get_time(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.TIME)
 
-    def get_enrolled(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.ENR)
+    def _get_enrolled(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.ENR)
 
-    def get_course_capacity(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.CAP)
+    def _get_course_capacity(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.CAP)
 
-    def get_faculty(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.FACULTY)
+    def _get_faculty(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.FACULTY)
 
-    def get_room(self, row: List[Dict[str, any]]) -> str:
-        return self.get_column(row, Columns.ROOM)
+    def _get_room(self, row: List[Dict[str, any]]) -> str:
+        return self.get_column(row, Column.ROOM)
 
     @staticmethod
-    def get_column(row: List[Dict[str, any]], column: Columns) -> str:
+    def get_column(row: List[Dict[str, any]], column: Column) -> str:
         cid = column.value
-        return row[cid]['text'].replace('\r', ' ')
-
+        temp = ''
+        try: 
+            temp = row[cid]['text'].replace('\r', ' ')
+        except Exception as e:
+            print(e)
+            # print(cid, row)
+            temp = "-"
+            
+        return temp
