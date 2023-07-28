0e0dd1a53d18528800f85881a7c75ccef7a35795
edoxa1 <vip.aidyn1@mail.ru>
2023-07-28 16:12:55 +0600

    added new command

--- tgbot/misc/set_base_commands.py
+++ tgbot/misc/set_base_commands.py
@@ -4,5 +4,6 @@ from aiogram.types import BotCommand
 
 async def set_default_commands(dp: Dispatcher):
     await dp.bot.set_my_commands([
-        BotCommand("start", "Start bot")
+        BotCommand("start", "Start bot"),
+        BotCommand("construct", "yes")
     ])
