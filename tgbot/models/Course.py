275a7465e95dc51657a1780ea8b603fe1c2e92de
edoxa1 <vip.aidyn1@mail.ru>
2023-07-28 16:16:18 +0600

    changed __init__

--- tgbot/models/Course.py
+++ tgbot/models/Course.py
@@ -4,22 +4,22 @@ class Course:
                  start_date, end_date, weekdays, time,
                  enrolled, course_capacity,
                  faculty, room):
-        self.abbr: str = abbr
-        self.course_type: str = course_type
-        self.title: str = title
-        self.credits_us: str = credits_us
-        self.credits_eu: str = credits_eu
-        self.start_date: str = start_date
-        self.end_date: str = end_date
-        self.weekdays: str = weekdays
-        self.time: str = time
-        self.enrolled: str = enrolled
-        self.course_capacity: str = course_capacity
-        self.faculty: str = faculty
+        self.abbr: str = abbr if abbr else 'N/A'
+        self.course_type: str = course_type if course_type else 'N/A'
+        self.title: str = title if title else 'N/A'
+        self.credits_us: str = credits_us if credits_us else 'N/A'
+        self.credits_eu: str = credits_eu if credits_eu else 'N/A'
+        self.start_date: str = start_date if start_date else 'N/A'
+        self.end_date: str = end_date if end_date else 'N/A'
+        self.weekdays: str = weekdays if weekdays else 'N/A'
+        self.time: str = time if time else 'N/A'
+        self.enrolled: str = enrolled if enrolled else 'N/A'
+        self.course_capacity: str = course_capacity if course_capacity else 'N/A'
+        self.faculty: str = faculty if faculty else 'N/A'
         try:
-            self.room: str = room.split('-')[0].strip()
+            self.room: str = room.split('-')[0].strip() if room else 'N/A'
         except ValueError:
-            self.room = room
+            self.room = room if room else 'N/A'
 
     def get_info(self) -> str:
         text = f'{self.abbr} [{self.course_type}] - {self.title} \n{self.credits_eu} ECTS\n' \
